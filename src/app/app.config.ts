994a557e003edcd24f4cdbc597f9a29a601ce61d
Sebastian Schult <sebastianschult@Sebastians-MacBook-Air.fritz.box>
2025-06-25 08:32:01 +0200

    add firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "huepfburgen-app", appId: "1:219430064885:web:e78e6bf0341eae866e2bbc", storageBucket: "huepfburgen-app.firebasestorage.app", apiKey: "AIzaSyD7kKQwv-kYD-zKFn6M_N1zfh9HzPMcWYE", authDomain: "huepfburgen-app.firebaseapp.com", messagingSenderId: "219430064885" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
